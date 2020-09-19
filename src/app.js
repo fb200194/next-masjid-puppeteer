046a9bb7e434a58dc1dcd9f183b984bc5b2fdaef
Fahmi Bnchi <fahmibnchi@gmail.com>
2020-09-19 18:59:24 +0300

    refactor

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
 const NextMasjidReport = require("./index.js");
